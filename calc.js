95fc089aa17a3ef5313ad36f6470a8d05e21f46b
JPCC <120706915+JPCC-p@users.noreply.github.com>
2022-12-25 08:13:44 +0900

    Add files via upload

--- calc.js
+++ calc.js
@@ -1,20 +1,28 @@
 function tscore(arr) {
-    console.log("data:",arr);
+    if (arr.length==null) {
+        console.error("data must be array");
+        return false;
+    }
+    if (arr.length<1) {
+        console.error("no data");
+        return false;
+    }
     let sum = arr.reduce((a,b)=>a+b); // summation
     let avr = sum/arr.length; // average
-    console.log("sum:",sum,"avr:",avr);
     let dev = []; // deviation
     let sdev = []; // squared deviation
     for (let elm of arr) {
         dev.push(elm-avr);
         sdev.push((elm-avr)**2);
     }
-    console.log("dev:",dev);
-    console.log("sdev:",sdev);
     let sdevsum = sdev.reduce((a,b)=>a+b);
     let vri = sdevsum/arr.length; // variance
     let stdev = Math.sqrt(vri); // standard deviation
-    console.log("vri",vri,"stdev:",stdev);
+    let tscore = [];
+    for (let elm of dev) {
+        tscore.push(elm*10/stdev+50)
+    }
+    return tscore;
 }
 
 exports.tscore = tscore;
